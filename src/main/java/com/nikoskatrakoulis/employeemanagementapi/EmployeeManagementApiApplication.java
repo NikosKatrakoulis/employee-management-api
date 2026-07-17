fadd255973e8cfd94e9183a4450bc3520c856bb3
NikosKatrakoulis <nikoskatrakoulis@gmail.com>
2026-07-17 09:00:25 +0300

    Initial project setup

---
+++ src/main/java/com/nikoskatrakoulis/employeemanagementapi/EmployeeManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.nikoskatrakoulis.employeemanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeManagementApiApplication.class, args);
+    }
+
+}
