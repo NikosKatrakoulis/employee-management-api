3874fd61602d5189d4b7c5313989123fd9ca7c18
NikosKatrakoulis <nikoskatrakoulis@gmail.com>
2026-07-18 08:22:48 +0300

    Refactoring EmployeeService.java

--- src/main/java/com/nikoskatrakoulis/employeemanagementapi/service/EmployeeService.java
+++ src/main/java/com/nikoskatrakoulis/employeemanagementapi/service/EmployeeService.java
@@ -1,13 +1,13 @@
 package com.nikoskatrakoulis.employeemanagementapi.service;
 
+import com.nikoskatrakoulis.employeemanagementapi.exception.EmployeeNotFoundException;
 import com.nikoskatrakoulis.employeemanagementapi.model.Employee;
 import com.nikoskatrakoulis.employeemanagementapi.repository.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+
 
 @Service
 @RequiredArgsConstructor
@@ -17,17 +17,36 @@ public class EmployeeService {
     private final EmployeeRepository employeeRepository;
 
     public List<Employee> getAllEmployees() {
-        List<Employee> employees = employeeRepository.findAll();
-        employees = new ArrayList<>(employees.size());
-        return employees;
+        return employeeRepository.findAll();
+
     }
 
     public Employee getEmployeeById(Long id) {
-        Optional<Employee> employee = this.employeeRepository.findById(id);
-        if (employee.isEmpty()) {
-            return null;
+        return employeeRepository.findById(id)
+                .orElseThrow(() -> new EmployeeNotFoundException("Employee not found with id: " + id));
+    }
+
+    public Employee createEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public Employee updateEmployee(Long id, Employee updateData) {
+        Employee existingEmployee = employeeRepository.findById(id)
+                .orElseThrow(() -> new EmployeeNotFoundException("Employee not found with id: " + id));
+        existingEmployee.setFirstName(updateData.getFirstName());
+        existingEmployee.setLastName(updateData.getLastName());
+        existingEmployee.setEmail(updateData.getEmail());
+        existingEmployee.setDepartment(updateData.getDepartment());
+        existingEmployee.setSalary(updateData.getSalary());
+
+        return employeeRepository.save(existingEmployee);
+    }
+
+    public void deleteEmployee(Long id) {
+        if (!employeeRepository.existsById(id)) {
+            throw new EmployeeNotFoundException("Employee not found with id: " + id);
         } else {
-            return employee.get();
+            employeeRepository.deleteById(id);
         }
     }
 }
