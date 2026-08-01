b74c5b462bb702994b659f42b7688189208a357f
NikosKatrakoulis <nikoskatrakoulis@gmail.com>
2026-08-01 11:49:14 +0300

    Adding app user table and seed for admin sql files

---
+++ src/main/resources/db/migration/V3__create_app_user_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE app_user (
+    id BIGSERIAL PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(255) NOT NULL
+);
---
+++ src/main/resources/db/migration/V4__seed_admin_user.sql
@@ -0,0 +1,2 @@
+INSERT INTO app_user(username, password, role)
+VALUES ('admin', '$2b$12$Qe9JYA8yDyqpmC1Ce9Jj3.BqRQ.f2pSa2PH2NX3JJ4Z7bOVomF6.6', 'ADMIN');
