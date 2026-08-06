d90d653116a98cc9c560d832d4da5bb7c7c64f46
NikosKatrakoulis <nikoskatrakoulis@gmail.com>
2026-08-06 19:20:06 +0300

    Update SecurityConfig.java

--- src/main/java/com/nikoskatrakoulis/employeemanagementapi/config/SecurityConfig.java
+++ src/main/java/com/nikoskatrakoulis/employeemanagementapi/config/SecurityConfig.java
@@ -3,7 +3,9 @@ package com.nikoskatrakoulis.employeemanagementapi.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -20,26 +22,22 @@ public class SecurityConfig {
     }
 
     @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login", "/errors", "/css/**").permitAll()
+                        .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/employees/**", "/api/departments/**").
                         permitAll()
                         .requestMatchers("/api/employees/**", "/api/departments/**", "/api/users/**")
                         .hasRole("ADMIN")
                         .anyRequest()
                         .authenticated()
-                )
-                .formLogin(form -> form
-                        .loginPage("/login")
-                        .defaultSuccessUrl("/employees", true)
-                        .permitAll()
-                )
-                .logout(logout-> logout
-                        .logoutSuccessUrl("/login?logout")
-                        .permitAll()
                 )
                 .httpBasic(Customizer.withDefaults());
 
